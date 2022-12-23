cdf2361ad8fa387c8db609a0357a5ae5628f91d5
josiregar <josadisiregar200109@gmail.com>
2022-12-23 14:01:58 +0700

    create mahasiswa

--- app/Http/Controllers/Api/MahasiswaController.php
+++ app/Http/Controllers/Api/MahasiswaController.php
@@ -7,5 +7,38 @@ use Illuminate\Http\Request;
 
 class MahasiswaController extends Controller
 {
-    //
+    //create data mahasiswa
+    public function create(Request $request)
+    {
+        //validasi data
+        $this->validate($request, [
+            'nama' => 'required',
+            'alamat' => 'required',
+            'tempat_lahir' => 'required',
+            'tanggal lahir' => 'required',
+            'npm' => 'required'
+        ]);
+
+        //membuat data mahasiswa
+        $mahasiswa = Mahasiswa::create([
+            'nama' => $request->nama,
+            'alamat' => $request->alamat,
+            'tempat_lahir' => $request->tempat_lahir,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'npm' => $request->npm
+        ]);
+        if($mahasiswa){
+            return response()->json([
+                'success' => true,
+                'message' => 'Mahasiswa berhasil ditambahkan',
+                'data' => $mahasiswa
+            ], 201);
+        }else{
+            return response()->json([
+                'success' => false,
+                'message' => 'Mahasiswa gagal ditambahkan',
+                'data' => ''
+            ], 400);
+        }
+    }
 }
